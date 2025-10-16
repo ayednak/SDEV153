a5db41b28d43ad1397554be9a409fe6d193a4f23
ayednak <165698483+ayednak@users.noreply.github.com>
2025-10-16 12:06:04 -0400

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,14 +1,31 @@
-<script>
-    form = document.getElementById('testimonialForm')
+let form = document.getElementById("testimonialForm");
+let nameToValidate = form.name.value;
+let messageToValidate = form.testimonial.value;
+let emailToValidate = form.email.value;
+let emailValidationRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-    form.addEventListener('submit', function(event) {
-        event.preventDefault(); 
+    
+function validateAndDisplay(event) {
+    if (nameToValidate.length < 2) {
+        event.preventDefault();
+        return alert("Name must be at least 2 characters long.");
+    }
+    else if (messageToValidate.length < 15) {
+        event.preventDefault();
+        return alert("Testimonial must be at least 15 characters long.");
+    }
+    else if (!emailValidationRegex.test(emailToValidate)) {
+        event.preventDefault();
+        return alert("Invalid email address format.");
+    }
+    else {
+        let validatedName = form.name.value;
+        let validatedMessage = form.testimonial.value;
 
-        const name = document.getElementById('name').value;
-        const message = document.getElementById('testimonial').value;
+        form.inputName.textContent = validatedName;
+        form.inputTestimonial.textContent = validatedMessage;
+        form.defaultTestimonial.textContent = "";
+    }
 
-        document.getElementById('inputName').textContent = name;
-        document.getElementById('inputTestimonial').textContent = message;
-    });
-</script>
-
+form.addEventListener('submit', validateAndDisplay);
+}
