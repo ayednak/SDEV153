2c4c38ca02e90ca211ff8b8fcdaad4e028ef8233
ayednak <165698483+ayednak@users.noreply.github.com>
2025-10-16 15:28:16 -0400

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,39 +1,9 @@
-function validateAndDisplay(event) {
-    let nameToValidate = document.getElementById("name").value;
-    alert(nameToValidate);
-    let messageToValidate = document.getElementById("testimonial").value;
-    alert
-    let emailToValidate = document.getElementById("email").value;
-    let emailValidationRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (nameToValidate.length < 2) {
-        event.preventDefault();
-        return alert("Name must be at least 2 characters long.");
-    }
-    else if (messageToValidate.length < 15) {
-        event.preventDefault();
-        return alert("Testimonial must be at least 15 characters long.");
-    }
-    else if (!emailValidationRegex.test(emailToValidate)) {
-        event.preventDefault();
-        return alert("Invalid email address format.");
-    }
-    else {
-       let validatedName = nameToValidate;
-       let validatedTestimonial = messageToValidate;
-       
-       let ouputName = document.getElementById("inputName");
-       let outputTestimonial = document.getElementById("inputTestimonial");
-       let defaultTestimonial = document.getElementById("defaultTestimonial");
-      
-       ouputName.textContent = validatedName;
-       outputTestimonial.textContent = validatedTestimonial;
-       defaultTestimonial.textContent = "";
-    }
-
 function functionTest(event) {
     alert("Event listener successfully called function!");
 }
+alert("Page successfully called scripts.js!");
+
 let form = document.getElementById("testimonialForm");
-form.addEventListener("submit", functionTest);
-form.addEventListener("submit", validateAndDisplay);
-}
+alert(form);
+form.addEventListener("onmouseover", functionTest);
+
