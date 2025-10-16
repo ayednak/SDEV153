13c47bb1837ec0d347c2473f7b9bc5d0feb0d90d
ayednak <165698483+ayednak@users.noreply.github.com>
2025-10-16 15:46:03 -0400

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
 function functionTest(event) {
     alert("Event listener successfully called function!");
 }
+
 alert("Page successfully called scripts.js!");
 
 let form = document.getElementById("testimonialForm");
-alert(form);
-form.addEventListener("onmouseover", functionTest);
+form.addEventListener("submit", functionTest);
 
